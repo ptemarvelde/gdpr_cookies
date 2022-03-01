66658b33903865d99d2e93456f9c417751201465
Adam <adam_school@outlook.com>
2022-03-01 23:06:42 +0100

    Added a begin for the banner detection functionality

---
+++ pythia/check_banner.py
@@ -0,0 +1,8 @@
+def iterate_over_websites(output_file):
+    with open(output_file) as file:
+        for line in file:
+            # add regularExpression
+
+
+if __name__ == "__main__":
+    iterate_over_websites("samples/example.json")
