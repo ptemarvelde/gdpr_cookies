d9335609424995d2457eab94c76bc9ce4e519992
ayush <S.A.S.patandin@student.tudelft.nl>
2022-03-02 19:51:01 +0100

    Added regex to search for banners in cookies

--- pythia/check_banner.py
+++ pythia/check_banner.py
@@ -1,7 +1,19 @@
+import json
+import re
+
+
 def iterate_over_websites(output_file):
+    res = []
+    patterns = ['Banner', 'banner']
     with open(output_file) as file:
         for line in file:
             # add regularExpression
+            text = json.loads(line)
+            for pattern in patterns:
+                elem = text['browser_module']['page_source']
+                if re.search(pattern, elem) and text not in res:
+                    res.append(text)
+        print(res)
 
 
 if __name__ == "__main__":
